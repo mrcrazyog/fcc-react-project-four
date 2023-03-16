ba5d1ca716e983204a840b5251db59220ecd5d6f
Jan Michalcak <jan@michalcak.eu>
2023-03-16 20:13:27 +0100

    fully functional app, without styling

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -11,6 +11,9 @@ function Calculator() {
   const [intermediateValue, setIntermediateValue] = useState('');
   const [decimalPressed, setDecimalPressed] = useState(false);
   const [operatorBeforeNegative, setOperatorBeforeNegative] = useState('');
+  const [pendingOperator, setPendingOperator] = useState('');
+  const [applyNegative, setApplyNegative] = useState(false);
+
   //   console.log(currentOperation, 'current operation');
 
   const digits = {
@@ -26,65 +29,73 @@ function Calculator() {
     9: 'nine',
   };
 
-  useEffect(() => {
-    console.log('The current operation is', currentOperation);
-  }, [currentOperation]);
-
   const handleNumberClick = (digit) => {
     if (input === '' && digit === '0') {
       setInput('');
     } else {
-      if (lastAction === 'negative' && operatorBeforeNegative) {
+      if (applyNegative) {
         setInput('-' + digit);
-        handleOperationClick(operatorBeforeNegative);
+        setApplyNegative(false);
       } else {
         setInput(input + digit);
       }
       setLastAction('number');
     }
     setOperatorPressed(false);
+    setPendingOperator('');
+  };
+
+  const handleNegativeClick = () => {
+    if (lastAction !== 'negative') {
+      handleOperationClick('-');
+    }
   };
 
   const handleOperationClick = (operator) => {
     if (operatorPressed && operator !== '-') {
-      setCurrentOperation(operator);
-      setOperatorBeforeNegative('');
+      setPendingOperator(operator);
       return;
     }
 
     if (operator === '-' && (lastAction === 'operator' || input === '')) {
       if (lastAction === 'negative') {
         setLastAction('operator');
-        setInput('');
+        setApplyNegative(false);
+        setOperatorPressed(true);
+        return;
       } else {
         setLastAction('negative');
-        setOperatorBeforeNegative(currentOperation);
+        setOperatorBeforeNegative(pendingOperator || currentOperation);
+        setPendingOperator('');
+        setApplyNegative(true);
+        return;
       }
-      setOperatorPressed(true);
-      return;
     }
 
-    if (input && lastAction === 'number') {
-      if (
-        intermediateValue !== 0 &&
-        (lastAction === 'number' || lastAction === 'equals')
-      ) {
-        let newResult = handleEqualsClick(input, intermediateValue);
-        setIntermediateValue(newResult);
-      } else {
-        setIntermediateValue(parseFloat(input));
-      }
-    } else if (lastAction === 'negative') {
-      setCurrentOperation(operatorBeforeNegative);
+    if (applyNegative && input) {
+      setInput('-' + input);
+      setApplyNegative(false);
+    }
+
+    if (intermediateValue !== 0 && input) {
+      let newResult = handleEqualsClick(input, intermediateValue);
+      setIntermediateValue(newResult);
+      setInput('');
+    } else if (input) {
+      setIntermediateValue(parseFloat(input));
+      setInput('');
     } else if (lastAction === 'equals') {
       setIntermediateValue(output);
     }
 
-    setInput('');
     setCurrentOperation(operator);
     setDecimalPressed(false);
     setLastAction('operator');
-    setOperatorPressed(true);
+    setOperatorPressed(false);
+    setPendingOperator(operator);
+    if (lastAction === 'negative') {
+      setApplyNegative(false);
+    }
   };
 
   const handleClearClick = () => {
@@ -130,6 +141,8 @@ function Calculator() {
         newResult = currentInputValue;
     }
 
+    setApplyNegative(false);
+
     setOutput(newResult);
     setInput('');
     setLastAction('equals');
@@ -157,7 +170,7 @@ function Calculator() {
       <CalculatorButton
         text='-'
         id='subtract'
-        onClick={() => handleOperationClick('-')}
+        onClick={() => handleNegativeClick()}
       />
       <CalculatorButton
         text='*'
